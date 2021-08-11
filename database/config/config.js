81082c048684138c3cbea0e2b608376112987cf1
Facundojs <facuserra2002@gmail.com>
2021-08-11 01:34:03 +0000

    cors

--- database/config/config.js
+++ database/config/config.js
@@ -13,14 +13,7 @@ module.exports = {
     "password": `Julian10`,
     "database": `kardiadev`,
     "host": `kardiadev.cs4901g4yrbg.us-east-1.rds.amazonaws.com`,
-    "dialect": "mysql"
-  },
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "kardia_back",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
   },
   "test": {
     "username": "root",
--- site/src/app.js
+++ site/src/app.js
@@ -5,15 +5,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin:["*"]
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
 }));
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(bodyParser.urlencoded({
     extended: false
