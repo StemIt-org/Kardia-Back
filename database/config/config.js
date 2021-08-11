047993005ecdd6f2042b6084bda6bb6105224a8d
Facundo Serrano <facuserra2002@gmail.com>
2021-08-10 22:37:32 -0300

    envs config

--- database/config/config.js
+++ database/config/config.js
@@ -1,11 +1,4 @@
 require('dotenv').config()
-const defaulDevelopsettings = {
-  "username": "root",
-  "password": null,
-  "database": "kardia_back",
-  "host": "127.0.0.1",
-  "dialect": "mysql"
-};
 
 module.exports = {
   "development": {
